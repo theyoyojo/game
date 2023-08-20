eb7c040cf4f1fc5ab8bbf75b649c4cbbe8bce8c4
Joel Savitz <joelsavitz@gmail.com>
2023-08-20 11:07:49 -0400

    implement basic game mechanics
    Signed-off-by: Joel Savitz <joelsavitz@gmail.com>

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ HTML="""
   </head>
   <body>
     <div id="header">
-      <img src="/data/US.png" />
-      <h1>Underground Software Clicker</h1>
+      <img id="logo" src="/data/US.png" />
+      <h1 id="title">Underground Software Clicker</h1>
       <br />
     </div>
     <hr />
@@ -30,11 +30,56 @@ HTML="""
             <td>Pickpockets</td>
             <td id="tab_ppkts">DEADBEEF</td>
           </tr>
+          <tr>
+            <td>Muggers</td>
+            <td id="tab_mugrs">DEADBEEF</td>
+          </tr>
+          <tr>
+            <td>Home Invaders</td>
+            <td id="tab_hinvs">DEADBEEF</td>
+          </tr>
+          <tr>
+            <td>Carjackers</td>
+            <td id="tab_cjcks">DEADBEEF</td>
+          </tr>
+          <tr>
+            <td>Bank Robbers</td>
+            <td id="tab_robrs">DEADBEEF</td>
+          </tr>
+          <tr>
+            <td>Mail Fraudsters</td>
+            <td id="tab_mafrs">DEADBEEF</td>
+          </tr>
+          <tr>
+            <td>Crypto Scammers</td>
+            <td id="tab_cscms">DEADBEEF</td>
+          </tr>
+          <tr>
+            <td>Darknet Moguls</td>
+            <td id="tab_dmogs">DEADBEEF</td>
+          </tr>
+          <tr>
+            <td>High Seas Pirates</td>
+            <td id="tab_hisps">DEADBEEF</td>
+          </tr>
+          <tr>
+            <td>Investment Bankers</td>
+            <td id="tab_ibers">DEADBEEF</td>
+          </tr>
         </table>
       </div>
       <div id="actions">
-        <button onclick="increment_coins()" id="bt_gutter">Search Gutter</button>
-        <button onclick="buy_pickpocket()" id="bt_ppkt">Buy Pickpocket for 10 coins</button>
+        <button onclick="steal()"               id="bt_steal">Steal</button>
+        <button onclick="buy_pickpocket()"      id="bt_ppkts">PICKPOCKET</button>
+        <button onclick="buy_mugger()"          id="bt_mugrs">MUGGER</button>
+        <button onclick="buy_home_invader()"    id="bt_hinvs">HOME INVADER</button>
+        <button onclick="buy_carjacker()"       id="bt_cjcks">CARJACKER</button>
+        <button onclick="buy_bank_robber()"     id="bt_robrs">BANK ROBBERS</button>
+        <button onclick="buy_mail_fraudster()"  id="bt_mafrs">MAIL FRAUDSTERS</button>
+        <button onclick="buy_crypto_scammer()"  id="bt_cscms">CRYPTO SCAMMERS</button>
+        <button onclick="buy_darknet_mogul()"  id="bt_dmogs">DARKNET MOGULS</button>
+        <button onclick="buy_high_seas_pirate()" id="bt_hisps">HIGH SEAS PIRATES</button>
+        <button onclick="buy_investment_banker()" id="bt_ibers">INVESTMENT BANKERS</button>
       </div>
     </div>
   <script src="/data/include.js" /></script>
--- data/include.js
+++ data/include.js
@@ -1,94 +1,142 @@
-
+/*
+ * Underground Software Clicker
+ *
+ * (C) 2023 Joel Savitz <joelsavitz@gmail.com>
+ *
+ */
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// The Game structure is the state of the game at any given time.
+// This initial state is the state of a new game.
+// rate := per-tick dollar earnings
+// bcost := base cost, price of first item
+// scheme: from level n to n+1, rate goes up by 5 as bcost goes up by 10
 Game = {
-	coins: {value:0.0, id:"tab_coins"},
-	ticks: {value:0, id:"tab_ticks"},
+	coins: {name: "Coins",		value:0.0, 	id: "tab_coins"},
+	ticks: {name: "Ticks",		value:0, 	id: "tab_ticks"},
 	// ppkts := pickpockets
-	ppkts: {value:0, id:"tab_ppkts"},
+	ppkts: {name: "Pickpockets",	value:0, 	id: "tab_ppkts", 	rate: 5 ** 0,	bcost: 10 ** 1},
+	// mugs := muggers
+	mugrs: {name: "Muggers",	value:0, 	id: "tab_mugrs",	rate: 5 ** 1,	bcost: 10 ** 2},
+	// hinvs := home invaders
+	hinvs: {name: "Home Invaders",	value:0, 	id: "tab_hinvs",	rate: 5 ** 2,	bcost: 10 ** 3},
+	// cjcks := car jackers
+	cjcks: {name: "Car Jackers",	value:0,	id: "tab_cjcks",	rate: 5 ** 3,	bcost: 10 ** 4},
+	// robrs := bank robbers
+	robrs: {name: "Bank Robbers", 	value:0,	id: "tab_robrs",	rate: 5 ** 4,	bcost: 10 ** 5},
+	// mafrs := mail fraudsters
+	mafrs: {name: "Mail Fraudsters",value:0, 	id: "tab_mafrs",	rate: 5 ** 5, 	bcost: 10 ** 6},
+	// cscms := crypto scammers
+	cscms: {name: "Crypto Scammers",value:0,	id: "tab_cscms",	rate: 5 ** 6,	bcost: 10 ** 7},
+	// dmogs := darknet moguls
+	dmogs: {name: "Darknet Moguls",	value:0,	id: "tab_dmogs",	rate: 5 ** 7,	bcost: 10 ** 8},
+	// hisps := high seas pirates
+	hisps: {name: "High Seas Pirates",value:0,	id: "tab_hisps",	rate: 5 ** 8,	bcost: 10 ** 9},
+	// ibers := investment bankers
+	ibers: {name: "Investment Bankers",value:0, 	id: "tab_ibers",	rate: 5 ** 9,	bcost: 10 ** 10},
+}
+
+// producers array is used to determine where to apply actions on all money-producing entities,
+// e.g. updating buttons, earning coins
+producers = ["ppkts", "mugrs", "hinvs", "cjcks", "robrs", "mafrs", "cscms", "dmogs", "hisps", "ibers"]
+
+function fixnumber(n) {
+	// get rid of extra bits after second decimal place
+	if (n % 1)
+		n = Math.round(n * 100)/100
+
+	// no really big numbers, where really big means log10(x) > 5
+	if (Math.log10(n) > 5)
+		n = n.toExponential(3)
+
+	return n
+}
+
+function price_check(item) {
+	entry = Game[item]
+	if (entry === null)
+		return -1.0
+
+	// for now we just scale cost by 2.5 per item purchased
+	res = fixnumber(entry.bcost * (2.5 ** entry.value))
+
+	return res
 }
 
 
-
-function update_ticks() {
-	number = document.getElementById("tab_ticks")
-	number.innerHTML = Game.tick.value
+function rate_check(item) {
+	return fixnumber(Game[item].rate)
 }
 
-function update_coins() {
-	number = document.getElementById("tab_coins")
-	number.innerHTML = Game.coins
+function try_buy(item) {
+	if (modify_coins(-1 * price_check(item)))
+		Game[item].value++
+	update_screen()
 }
 
-function update_pickpockets() {
-	number = document.getElementById("tab_pickpockets")
-	number.innerHTML = Game.ppkts
-}
+function buy_pickpocket() 	{try_buy("ppkts")}
+function buy_mugger() 		{try_buy("mugrs")}
+function buy_home_invader() 	{try_buy("hinvs")}
+function buy_carjacker() 	{try_buy("cjcks")}
+function buy_bank_robber() 	{try_buy("robrs")}
+function buy_mail_fraudster() 	{try_buy("mafrs")}
+function buy_crypto_scammer() 	{try_buy("cscms")}
+function buy_darknet_mogul() 	{try_buy("dmogs")}
+function buy_high_seas_pirate() {try_buy("hisps")}
+function buy_investment_banker(){try_buy("ibers")}
 
-function price_pickpockets() {
-	return 10 * (2.5 ** Game.ppkts.value)
-}
-
-function button_text_pickpockets() {
-	return "Buy pickpocket (1 coin/s) for " + price_pickpockets() + " coins"
+function button_text(item) {
+	e = Game[item]
+	return "Buy " + e.name + " (" + rate_check(item) + " coin/s) for " + price_check(item) + " coins"
 }
 
 function update_screen() {
 	// update values of main Game items
 	for (const item in Game) {
 		target = document.getElementById(Game[item].id)
-		target.innerHTML = Game[item].value
+		target.innerHTML = fixnumber(Game[item].value)
 	}
 
-	bt = document.getElementById("bt_ppkt")
-	i = 10
-	// while (bt.innerHTML[i] != '>' && i < 100)
-	// 	i++;
-	bt.innerHTML = button_text_pickpockets()
-
-	//update text box
-	//target = document.t
+	for (key of producers) {
+		btname = "bt_" + key
+		bt = document.getElementById(btname)
+		bt.innerHTML = button_text(key)
+		if (price_check(key) > Game.coins.value)
+			bt.disabled = "true"
+		else
+			bt.disabled = ""
+	}
 }
 
 // don't allow coins to fall below 0
 function modify_coins(x) {
-	if (get_coins() + x < 0)
+	if (Game.coins.value + x < 0)
 		return false
 
 	Game.coins.value += x
 	return true
 }
 
-function get_coins() {
-	return Game.coins.value
-}
 
-
-function increment_coins() {
+// rob from the poor and give to the rich
+function steal() {
 	modify_coins(1)
 	update_screen()
 }
 
-function increment_pickpockets() {
-	Game.ppkts.value++
-}
-
-function buy_pickpocket() {
-	if (modify_coins(-1 * price_pickpockets())) {
-		increment_pickpockets()
-	}
-	update_screen()
-}
-
 function earn_coins_for_tick() {
-	// for now, ppkts give you 1 coin
-	earned = Game.ppkts.value
+	earned = 0
+
+	// earn the per-tick rate of each producer for each tier
+	for (key of producers)
+		earned += Game[key].value * Game[key].rate
 
 	Game.coins.value += earned
 }
 
+// This main game loop runs forever on page load.
 async function tick_loop() {
 	for (;;) {
 		Game.ticks.value++
